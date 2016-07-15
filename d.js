f922dff78157eb86f6b452d33f922cdfa2af05f6
Virlyz Nyzam <samsury08@gmail.com>
2016-07-15 03:08:47 -0700

    Update d.js

--- d.js
+++ d.js
@@ -23,3 +23,28 @@ var disqus_url = disqus_blogger_current_url;
         (document.getElementsByTagName('head')[0] || document.body).appendChild(dsq);
     }
 })();
+    var disqus_loaded = false;
+    function load_disqus() {
+(function() {
+    var bloggerjs = document.createElement('script');
+    bloggerjs.type = 'text/javascript';
+    bloggerjs.async = true;
+    bloggerjs.src = '//'+disqus_shortname+'.disqus.com/blogger_item.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
+})();
+(function() {
+    var bloggerjs = document.createElement('script');
+    bloggerjs.type = 'text/javascript';
+    bloggerjs.async = true;
+    bloggerjs.src = '//'+disqus_shortname+'.disqus.com/blogger_index.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
+})();
+    disqus_loaded = true;
+    };
+    window.onscroll = function(e) {
+        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 800)) {
+            //hit bottom of page
+            if (disqus_loaded==false)
+                load_disqus()
+        }
+    };
